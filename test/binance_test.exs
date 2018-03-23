b78279b99bf842d86da4a40f5cd1037635827274
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-03-22 22:57:14 -0700

    Add missing test coverage for existing public API

--- test/binance_test.exs
+++ test/binance_test.exs
@@ -7,6 +7,49 @@ defmodule BinanceTest do
     HTTPoison.start
   end
 
+  test "ping returns an empty map" do
+    use_cassette "ping_ok" do
+      assert Binance.ping() == {:ok, %{}}
+    end
+  end
+
+  test "get_server_time success return an ok, time tuple" do
+    use_cassette "get_server_time_ok" do
+      assert Binance.get_server_time() == {:ok, 1521781361467}
+    end
+  end
+
+  test "get_all_prices returns a list of prices for every symbol" do
+    use_cassette "get_all_prices_ok" do
+      assert {:ok, symbol_prices} = Binance.get_all_prices()
+      assert [%Binance.SymbolPrice{price: "0.06137000", symbol: "ETHBTC"} | _tail] = symbol_prices
+      assert symbol_prices |> Enum.count == 288
+    end
+  end
+
+  test "get_ticker returns a ticker struct with details for the given symbol" do
+    use_cassette "get_ticker_ok" do
+      assert {
+        :ok,
+        %Binance.Ticker{
+          ask_price: "0.01876000",
+          bid_price: "0.01875200",
+          close_time: 1521826338547,
+          count: 30612
+        }
+      } = Binance.get_ticker("LTCBTC")
+    end
+  end
+
+  test "get_ticker returns an error tuple when the symbol doesn't exist" do
+    use_cassette "get_ticker_error" do
+      assert Binance.get_ticker("IDONTEXIST") == {
+        :error,
+        %{"code" => -1121, "msg" => "Invalid symbol."}
+      }
+    end
+  end
+
   test "get_depth returns the bids & asks up to the given depth" do
     use_cassette "get_depth_ok" do
       assert Binance.get_depth("BTCUSDT", 5) == {
