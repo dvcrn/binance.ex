35277f815bbf44cb88dfab14ffb8fadaab7b5c5b
David Mohl <git@davemail.io>
2018-08-13 10:42:26 +0900

    Release 0.6.0, closes #21

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Elixir wrapper for interacting with the [Binance API](https://github.com/binance
 ```elixir
 def deps do
   [
-    {:binance, "~> 0.5.0"}
+    {:binance, "~> 0.6.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binance.MixProject do
   def project do
     [
       app: :binance,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
