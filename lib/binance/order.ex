19c2249b13e91faf72c2877fe7fc676a45a6a197
Kamil Skowron <kamilskowron@gambitresearch.com>
2019-05-01 12:57:24 +0100

    get_open_order functionality

---
+++ lib/binance/order.ex
@@ -0,0 +1,26 @@
+defmodule Binance.Order do
+  @moduledoc """
+  Struct for representing the result returned by /api/v3/openOrders
+  """
+
+  defstruct [
+    :symbol,
+    :order_id,
+    :client_order_id,
+    :price,
+    :orig_qty,
+    :executed_qty,
+    :cummulative_quote_qty,
+    :status,
+    :time_in_force,
+    :type,
+    :side,
+    :stop_price,
+    :iceberg_qty,
+    :time,
+    :update_time,
+    :is_working
+  ]
+
+  use ExConstructor
+end
