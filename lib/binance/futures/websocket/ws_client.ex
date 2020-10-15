b480e04a810466afd2a040e92cd1aca8362205a1
Long Nguyen <long.polyglot@gmail.com>
2020-10-15 16:34:14 +0700

    Update WS config

--- lib/binance/futures/websocket/ws_client.ex
+++ lib/binance/futures/websocket/ws_client.ex
@@ -39,7 +39,7 @@ defmodule Binance.Futures.WebSocket.WSClient do
   defmacro __using__(_opts) do
     quote do
       use WebSockex
-      @base Application.get_env(:binance, :ws_endpoint, "wss://fstream.binance.com")
+      @base Application.get_env(:binance, :ws_futures_endpoint, "wss://fstream.binance.com")
       @ping_interval Application.get_env(:binance, :ping_interval, 5_000)
       @keep_alive_interval Application.get_env(:binance, :keep_alive_interval, 10 * 60_000)
 
