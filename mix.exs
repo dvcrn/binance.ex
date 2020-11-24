67727a243b46f710526a2785f1b3499776960e6d
simplem4n <quan.tran@streamy.live>
2020-11-24 14:49:21 +0700

    fix warning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binance.MixProject do
   def project do
     [
       app: :binance,
-      version: "0.10.0",
+      version: "0.10.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
