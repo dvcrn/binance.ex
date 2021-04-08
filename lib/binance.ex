c7e127b6a95b6e721716f7540157c3a47ac88186
kitplummer <kit.plummer@gtri.gatech.edu>
2021-04-08 10:25:49 -0400

    fix: clean up doc, remove dead code

--- lib/binance.ex
+++ lib/binance.ex
@@ -98,27 +98,38 @@ defmodule Binance do
   end
 
   @doc """
-  Retrieves klines for a symbol, provided a given interval
+  Retrieves klines for a symbol, provided a given interval, e.g. "1h".
+
+  Function can also take in a 'limit' argument to reduce the number of intervals.
+
+  Returns `{:ok, [%Binance.Kline{}]` or `{:error, reason}`
+
+  ## Example
+  ```
+  {:ok,
+  [
+   %Binance.Kline{
+     close: "0.16527000",
+     close_time: 1617861599999,
+     high: "0.17100000",
+     ignore: "0",
+     low: "0.16352000",
+     number_of_trades: 16167,
+     open: "0.17088000",
+     open_time: 1617858000000,
+     quote_asset_volume: "7713624.32966000",
+     taker_buy_base_asset_volume: "22020677.70000000",
+     taker_buy_quote_asset_volume: "3668705.43042700",
+     volume: "46282422.20000000"
+   },
+   %Binance.Kline{
+   ...
+   ```
   """
 
   def get_klines(symbol, interval, limit \\ 500) when is_binary(symbol) do
     case HTTPClient.get_binance("/api/v3/klines?symbol=#{symbol}&interval=#{interval}&limit=#{limit}") do
       {:ok, data} ->
-        #IO.inspect data
-        # opts = [
-        #   open_time: elem(data, 0),
-        #   open: elem(data, 1),
-        #   high: elem(data, 2),
-        #   low: elem(data, 3),
-        #   close: elem(data, 4),
-        #   volume: elem(data, 5),
-        #   close_time: elem(data, 6),
-        #   quote_asset_volume: elem(data, 7),
-        #   number_of_trades: elem(data, 8),
-        #   taker_buy_base_asset_volume: elem(data, 9),
-        #   taker_buy_quote_asset_volume: elem(data, 10),
-        #   ignore: elem(data, 11)
-        # ]
         {:ok, Enum.map(data, &Binance.Kline.new(&1))}
       err -> err
     end
