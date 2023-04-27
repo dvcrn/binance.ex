afcee1eaea9a25806aec34fd37d8a0df3dd0ace2
Kamil Skowron <emanuel1989@gmail.com>
2023-04-27 20:48:15 +0100

    Mix version update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binance.MixProject do
   def project do
     [
       app: :binance,
-      version: "1.0.1",
+      version: "1.0.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
