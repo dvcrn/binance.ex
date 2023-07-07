da76fb0e390e81f79507c6bd60f72cdb8ca9c03f
David Mohl <git@d.sh>
2023-07-07 16:16:00 +1000

    Remove beta flag

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binance.MixProject do
   def project do
     [
       app: :binance,
-      version: "2.0.0-beta.1",
+      version: "2.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
