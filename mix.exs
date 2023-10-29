a3be50985cb08f113b38b2bb55e9745ff0d0b060
David Mohl <git@d.sh>
2023-10-29 11:39:53 +0900

    Bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Binance.MixProject do
     [
       app: :binance,
       version: "2.0.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
